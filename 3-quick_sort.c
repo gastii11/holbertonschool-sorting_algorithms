3f3c0ae9d03b2b9cba03cecd91d7ac5cf75fd028
gastii11 <10632@holbertonstudents.com>
2025-04-11 18:47:55 +0000

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -16,7 +16,6 @@ void quicksort_recursivo(int *array, int low, int high, size_t size)
 		pivot = array[high];
 		i = low;
 
-
 		for (j = low; j < high; j++)
 		{
 			if (array[j] < pivot)
@@ -26,23 +25,21 @@ void quicksort_recursivo(int *array, int low, int high, size_t size)
 					temp = array[i];
 					array[i] = array[j];
 					array[j] = temp;
-					print_array(array, size);
 				}
-			i++;
+				i++;
 			}
 		}
+		if (i != high)
+		{
+			temp = array[i];
+			array[i] = array[high];
+			array[high] = temp;
+		}
 
-
-	if (i != high)
-	{
-		temp = array[i];
-		array[i] = array[high];
-		array[high] = temp;
 		print_array(array, size);
-	}
 
-	quicksort_recursivo(array, low, i - 1, size);
-	quicksort_recursivo(array, i + 1, high, size);
+		quicksort_recursivo(array, low, i - 1, size);
+		quicksort_recursivo(array, i + 1, high, size);
 	}
 }
 
