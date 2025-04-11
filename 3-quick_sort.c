e8722a2c044661369e91d2fd2b7440626d1d0d82
MaximoDilacio <developermaximo1@gmail.com>
2025-04-11 16:26:19 +0000

    File betty fix 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,42 +9,41 @@
  */
 void quicksort_recursivo(int *array, int low, int high, size_t size)
 {
-    int pivot, i, j, temp;
+	int pivot, i, j, temp;
 
-    if (low < high)
-    {
-        pivot = array[high];
-        i = low;
+	if (low < high)
+	{
+		pivot = array[high];
+		i = low;
 
-        /* Partición */
-        for (j = low; j < high; j++)
-        {
-            if (array[j] < pivot)
-            {
-                if (i != j)
-                {
-                    temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                    print_array(array, size);
-                }
-                i++;
-            }
-        }
 
-        /* Intercambio del pivote */
-        if (i != high)
-        {
-            temp = array[i];
-            array[i] = array[high];
-            array[high] = temp;
-            print_array(array, size);
-        }
+		for (j = low; j < high; j++)
+		{
+			if (array[j] < pivot)
+			{
+				if (i != j)
+				{
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+					print_array(array, size);
+				}
+			i++;
+			}
+		}
 
-        /* Llamadas recursivas */
-        quicksort_recursivo(array, low, i - 1, size);
-        quicksort_recursivo(array, i + 1, high, size);
-    }
+
+	if (i != high)
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+
+	quicksort_recursivo(array, low, i - 1, size);
+	quicksort_recursivo(array, i + 1, high, size);
+	}
 }
 
 /**
