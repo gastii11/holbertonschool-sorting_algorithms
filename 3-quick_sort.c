cf8fb10843aec2084bdd2d7e582b531c631ed864
gastii11 <10632@holbertonstudents.com>
2025-04-11 19:51:34 +0000

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -25,7 +25,7 @@ void quicksort_recursivo(int *array, int low, int high, size_t size)
 					temp = array[i];
 					array[i] = array[j];
 					array[j] = temp;
-
+					print_array(array, size);
 				}
 				i++;
 			}
@@ -35,7 +35,7 @@ void quicksort_recursivo(int *array, int low, int high, size_t size)
 			temp = array[i];
 			array[i] = array[high];
 			array[high] = temp;
-			print_array(array, size);
+
 		}
 
 
